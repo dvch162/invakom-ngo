f9a32c8c8fc30f90e9ce5d0b2adaae336a9e6750
dvch162 <deefordis8@gmail.com>
2023-05-25 16:27:05 +0300

    Payment form change

--- pages/PortmonePaymentForm.js
+++ pages/PortmonePaymentForm.js
@@ -2,18 +2,19 @@ import { useState } from 'react';
 import { deflate } from 'pako';
 import { encode } from 'js-base64';
 import { useRouter } from 'next/router';
-import Layout from '../components/Layout';
 
 export default function PortmonePaymentForm() {
   const [email, setEmail] = useState('');
   const [amount, setAmount] = useState('');
+  const [currency, setCurrency] = useState('uah'); // Default currency is UAH
   const router = useRouter();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = {
       email,
-      amount
+      amount,
+      currency,
     };
     const jsonData = JSON.stringify(data);
     const compressedData = deflate(jsonData);
@@ -23,18 +24,60 @@ export default function PortmonePaymentForm() {
   };
 
   return (
-    <Layout>
-      <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="payment-form">
+      <h1>Підтримайте нашу місію!</h1>
+      <div className="row">
         <label>
-          Email:
+          Ел. пошта:
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
         <label>
-          Amount:
+          Кількість:
           <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
         </label>
-        <button type="submit">Pay</button>
-      </form>
-    </Layout>
+        <label>
+          Валюта:
+          <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+            <option value="uah">UAH</option>
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+          </select>
+        </label>
+        <button type="submit">Сплатити</button>
+      </div>
+      <style jsx>{`
+        .payment-form {
+          display: flex;
+          flex-direction: column;
+          gap: 5px;
+          align-items: flex-start;
+        }
+
+        .row {
+          display: flex;
+          gap: 5px;
+        }
+
+        .payment-form h1 {
+          margin-bottom: 10px;
+          align-self: center;
+        }
+
+        @media (max-width: 600px) {
+          .payment-form {
+            flex-wrap: wrap;
+          }
+
+          .payment-form label,
+          .payment-form button {
+            width: 100%;
+          }
+
+          .payment-form button {
+            height: 2rem; /* Adjust the height as needed */
+          }
+        }
+      `}</style>
+    </form>
   );
 }
